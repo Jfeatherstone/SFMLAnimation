f1f88cae037b16fff68e68fc85ae698dcb12498d
Jack <Jfeatherstone123@gmail.com>
2018-12-04 09:48:18 -0500

    initial commit

---
+++ src/AnimationManager.cpp
@@ -0,0 +1,161 @@
+#include "AnimationManager.hpp"
+
+/*
+This default constructor isn't entirely necessary, but at least for now, here
+it is
+*/
+AnimationManager::AnimationManager() {
+
+}
+
+/*
+update(string animation, Sprite& sprite)
+
+animation - This is the string key that corresponds to the animation we are updating
+
+sprite - This sprite will have its texture changed to the next one in its
+  animation sequence
+*/
+void AnimationManager::update(string animation, Sprite &sprite) {
+  // First, we want to locate our animation in the map
+  // We look at the sheet size because that is the easiest indicator to tell if
+  // there is actually an entry
+  if (m_sheetSizes[animation] != Vector2i(0, 0)) {
+    // We want to do a few calculations to find the coordinates of the next frame
+    IntRect rect(m_indicies[animation].x * m_spriteSizes[animation].x,
+       m_indicies[animation].y * m_spriteSizes[animation].y,
+        m_spriteSizes[animation].x, m_spriteSizes[animation].y);
+
+    // Now we want to update the indicies based on the format of our sheet
+    // If we are not at the bottom of a column, we just move down one in y
+    if (m_indicies[animation].y < m_sheetSizes[animation].y) {
+      m_indicies[animation].y++;
+    } else {
+      // Otherwise, we move over one column and go back to the top
+      m_indicies[animation].y = 0;
+      m_indicies[animation].x++;
+      // And then reset the sheet if we are past the width of the sheet
+      if (m_indicies[animation].x >= m_sheetSizes[animation].x)
+        m_indicies[animation].x = 0;
+    }
+
+    // Now we update the texture on our sprite reference
+    sprite.setTexture(m_textures[animation]);
+    sprite.setTextureRect(rect);
+
+  } else {
+    // If we didn't find an entry
+    cout << "No animation entry found for \"" << animation << "\"!" << endl;
+  }
+}
+
+/*
+updateAll(map<string, Sprite> &map)
+
+map - This should be a map of string and sprite pairs, all of which that are found
+  in the local map will be updated
+
+This is essentially just an extension of the previous method, in that it just
+iterates through
+*/
+void AnimationManager::updateAll(map<string, Sprite> &map) {
+  // Really not much to do here, most is in the previous method
+  for (auto element: map) {
+    update(element.first, element.second);
+  }
+}
+
+/*
+ADDING & DELETING
+The following methods are for adding and deleting entries and animations
+*/
+
+/*
+addAnimation(string animation, Texture texture)
+
+animation - The string key for the animation we are adding
+texture - The spritesheet that will be associated with our animation
+sheetSize - The vector containing the number of animation frames in our image
+  - NOTE: all sheets must be rectangular
+*/
+void AnimationManager::addAnimation(string animation, Texture texture,
+   Vector2i sheetSize, Vector2i spriteSize, Vector2i index, int frequency,
+   Vector2i startingIndex) {
+  // First, we want to make an entry in the texture map
+  m_textures[animation] = texture;
+  // Next, we make sheet size entry
+  m_sheetSizes[animation].x = sheetSize.x;
+  m_sheetSizes[animation].y = sheetSize.y;
+
+  // Next, we make sprite size entry
+  m_spriteSizes[animation].x = spriteSize.x;
+  m_spriteSizes[animation].y = spriteSize.y;
+
+  // Our index vector
+  m_indicies[animation].x = index.x;
+  m_indicies[animation].y = index.y;
+
+  // Our starting index vector
+  m_startingIndicies[animation].x = startingIndex.x;
+  m_startingIndicies[animation].y = startingIndex.y;
+
+  // Our update rate (frequency)
+  m_frequencies[animation] = frequency;
+  // We don't need to initialize the times udpated because its default is 0
+}
+
+/*
+deleteAnimation(string animation)
+
+animation - The string key for the animation set we are removing
+
+Essentially this method just removes all entries for the animation provided
+If this is not called, there won't be a functional difference, but it *may*
+speed up the time it takes to run certain methods
+*/
+
+void AnimationManager::deleteAnimation(string animation) {
+  // We just erase each entry in every map
+  m_textures.erase(animation);
+  m_indicies.erase(animation);
+  m_startingIndicies.erase(animation);
+  m_sheetSizes.erase(animation);
+  m_spriteSizes.erase(animation);
+  m_frequencies.erase(animation);
+  m_timesUpdated.erase(animation);
+  // Ez pz
+}
+
+/*
+Here are a whole bunch of setters for any given animation instance
+Nothing too special about any of them in particular, and they all follow the
+same form
+*/
+
+void AnimationManager::setAnimationFrequency(string animation, int frequency) {
+  m_frequencies[animation] = frequency;
+}
+
+void AnimationManager::setAnimationIndex(string animation, Vector2i index) {
+  m_indicies[animation].x = index.x;
+  m_indicies[animation].y = index.y;
+}
+
+void AnimationManager::setAnimationSheetSize(string animation, Vector2i size) {
+  m_sheetSizes[animation].x = size.x;
+  m_sheetSizes[animation].y = size.y;
+}
+
+void AnimationManager::setAnimationSpriteSize(string animation, Vector2i size) {
+  m_spriteSizes[animation].x = size.x;
+  m_spriteSizes[animation].y = size.y;
+}
+
+void AnimationManager::setAnimationTexture(string animation, Texture texture) {
+  m_textures[animation] = texture;
+}
+
+void AnimationManager::resetAnimationIndex(string animation) {
+  m_indicies[animation].x = m_startingIndicies[animation].x;
+  m_indicies[animation].y = m_startingIndicies[animation].y;
+}
---
+++ src/AnimationManager.hpp
@@ -0,0 +1,136 @@
+/*
+MIT License
+
+Copyright (c) 2018 Jack Featherstone
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+
+DEPENDENCIES:
+std::string
+std::map
+sf::Sprite
+
+*/
+
+#pragma once
+#include <SFML/Graphics.hpp>
+#include <map>
+#include <string>
+#include <iostream>
+
+using namespace std;
+using namespace sf;
+/*
+
+SPECIFICATIONS:
+SFML 2.5.1
+2018 Q4
+
+This class, which is to be used in conjunction with the SFML framework, will
+help to smoothly integrate animation into a large scale game or other application.
+
+Many of the functions of this class will not be especially complicated, though
+they will hopefully simplify and increase readability for simple actions such
+as character walking/moving animations, jumping animatimations, etc.
+
+The basis of the code below will be the sf::Sprite class, the current documentation
+for which is linked below:
+
+https://www.sfml-dev.org/documentation/2.5.1/classsf_1_1Sprite.php
+
+*/
+
+class AnimationManager {
+/*
+The majority of information in this class will be contained in map types,
+where the key will be a user defined string identifying what type of animation
+is defiend there (walking, jumping, etc.)
+*/
+private:
+  /*
+  Since we don't know exactly what actions will be animated with this class, we
+  will allow the user to define their own identifiable sprite sheets
+  */
+  map<string, Texture> m_textures;
+  /*
+  We will want to keep track of where we are in the spritesheet, again for each
+  user-defined sheet
+  We use a vector such that >1 column spritesheets can be used easily
+  In the case that we are using a 1 column sheet, we should be able to tell through
+  the size, and ignore the second index
+  */
+  map<string, Vector2i> m_indicies;
+
+  /*
+  Since some spritesheets will contain multiple sets of animations, we want to
+  define a start index for all of our entries (though it may just be (0,0) for most)
+  */
+  map<string, Vector2i> m_startingIndicies;
+
+  // The former will be for the number of frames in a sheet, while the latter
+  // is the actual size of each frame in the sheets
+  map<string, Vector2i> m_sheetSizes;
+  map<string, Vector2i> m_spriteSizes;
+
+  /*
+  Since we will assume that every sheet will be updated every frame, we will
+  allow for a frequency to be set such that we can control the speed of animations
+
+  This values will be integers of the form where 1 (or 0) means it is updated
+  every call, 2 is updated every other call, etc.
+  */
+  map<string, int> m_frequencies;
+
+  /*
+  Because of the frequency issue, we also have to keep track of how many times
+  we have updated our sprites
+  */
+  map<string, int> m_timesUpdated;
+public:
+  AnimationManager();
+
+  void update(string animation, Sprite& sprite);
+
+  /*
+  This method may not be used, as it would require the game to organize all of
+  the animated sprites in a map, though it could be useful in a few situations,
+  so there's no harm in programming it now (it shouldn't be that hard)
+  */
+  void updateAll(map<string, Sprite>& map);
+
+  /*
+  The overloaded methods below will be the main functionality as far as adding
+  animations to the manager. The first will take in all necessary variables and
+  create the entry at the same time, while the second will require supplementation
+  by the setter methods below, otherwise they will maintain default values
+  */
+  void addAnimation(string animation, Texture texture, Vector2i sheetSize,
+     Vector2i spriteSize, Vector2i index = Vector2i(0, 0), int frequency = 0,
+    Vector2i startingIndex = Vector2i(0, 0));
+
+  void deleteAnimation(string animation);
+
+  void setAnimationFrequency(string animation, int frequency);
+  void setAnimationSpriteSize(string animation, Vector2i size);
+  void setAnimationSheetSize(string animation, Vector2i size);
+  void setAnimationIndex(string animation, Vector2i index);
+  void setAnimationTexture(string animation, Texture texture);
+
+  void resetAnimationIndex(string animation);
+};
